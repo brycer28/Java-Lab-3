0294529349a8c07b948c8902d824332b8e366912
brycer28 <brycer2825@gmail.com>
2024-10-16 17:30:46 -0500

    CSV file is now being read properly. CSV file also updated to fill in null values

--- src/DataVisTool.java
+++ src/DataVisTool.java
@@ -1,4 +1,44 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
 // reads data creates frame for various panels
 public class DataVisTool {
+    public static void main(String[] args) throws IOException {
+        ArrayList<PlayerStats> data = FileReader.readPlayerStat("UARK_Basketball_Stats_2019.csv");
 
+        for (PlayerStats playerStats : data) {
+            printPlayerStats(playerStats);
+        }
+    }
+
+    public static void printPlayerStats(PlayerStats data) {
+        System.out.println("2019 Basketball Season Statistics");
+        System.out.println("Rank:\t" + data.rank());
+        System.out.println("Name:\t" + data.name());
+        System.out.println("GP:\t\t" + data.GP());
+        System.out.println("GS:\t\t" + data.GS());
+        System.out.println("MP:\t\t" + data.MP());
+        System.out.println("FG:\t\t" + data.FG());
+        System.out.println("FGA:\t" + data.FGA());
+        System.out.println("FGP:\t" + data.FGP());
+        System.out.println("FG2:\t" + data.FG2());
+        System.out.println("FG2A:\t" + data.FG2A());
+        System.out.println("FG2P:\t" + data.FG2P());
+        System.out.println("FG3:\t" + data.FG3());
+        System.out.println("FG3A:\t" + data.FG3A());
+        System.out.println("FG3P:\t" + data.FG3P());
+        System.out.println("FT:\t\t" + data.FT());
+        System.out.println("FTA:\t" + data.FTA());
+        System.out.println("FTP:\t" + data.FTP());
+        System.out.println("ORB:\t" + data.ORB());
+        System.out.println("DRB:\t" + data.DRB());
+        System.out.println("TRB:\t" + data.TRB());
+        System.out.println("AST:\t" + data.AST());
+        System.out.println("STL:\t" + data.STL());
+        System.out.println("BLK:\t" + data.BLK());
+        System.out.println("TOV:\t" + data.TOV());
+        System.out.println("PF:\t\t" + data.PF());
+        System.out.println("PTS:\t" + data.PTS());
+        System.out.println("\n");
+    }
 }
--- src/FileReader.java
+++ src/FileReader.java
@@ -1,4 +1,60 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 // reads a .csv file of data and populates a PlayerStats object
 public class FileReader {
 
+    public static ArrayList<PlayerStats> readPlayerStat(String filename) throws IOException {
+        String contents = Files.readString(Path.of(filename), StandardCharsets.UTF_8);
+        Scanner scanner = new Scanner(contents);
+        ArrayList<PlayerStats> playerStats = new ArrayList<>();
+        scanner.nextLine();
+        scanner.useDelimiter(",");
+
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine().trim();
+            if (line.isEmpty()) {continue;}
+            String[] data = line.split(",");
+            try {
+                PlayerStats datum = new PlayerStats(
+                        Byte.parseByte(data[0]),    //rank
+                        data[1],                    //name
+                        Byte.parseByte(data[2]),    //GP
+                        Byte.parseByte(data[3]),    //GS
+                        Integer.parseInt(data[4]),  //MP
+                        Integer.parseInt(data[5]),  //FG
+                        Integer.parseInt(data[6]),  //FGA
+                        Float.parseFloat(data[7]),  //FGP
+                        Integer.parseInt(data[8]),  //FG2
+                        Integer.parseInt(data[9]),  //FG2A
+                        Float.parseFloat(data[10]),  //FG2P
+                        Integer.parseInt(data[11]),  //FG3
+                        Integer.parseInt(data[12]),  //FG3A
+                        Float.parseFloat(data[13]),  //FG3P
+                        Integer.parseInt(data[14]),  //FT
+                        Integer.parseInt(data[15]),  //FTA
+                        Float.parseFloat(data[16]),  //FTP
+                        Integer.parseInt(data[17]),  //ORB
+                        Integer.parseInt(data[18]),  //DRB
+                        Integer.parseInt(data[19]),  //TRB
+                        Integer.parseInt(data[20]),  //AST
+                        Integer.parseInt(data[21]),  //STL
+                        Integer.parseInt(data[22]),  //BLK
+                        Integer.parseInt(data[23]),  //TOV
+                        Integer.parseInt(data[24]),  //PF
+                        Integer.parseInt(data[25])   //PTS
+                );
+                playerStats.add(datum);
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                continue;
+            }
+        }
+        scanner.close();
+        return playerStats;
+    }
 }
